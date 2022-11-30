6f25b2427f80260238db5cf3a5ba4abb7eecf39f
tonjehusvik <tonje.husvik@hotmail.com>
2022-11-30 23:12:46 +0100

    Alarm should be working

--- infra/sns_topic_for_alarm.tf
+++ infra/sns_topic_for_alarm.tf
@@ -1,3 +1,17 @@
+resource "aws_cloudwatch_metric_alarm" "zerosum" {
+  alarm_name                = "cart-over-5"
+  namespace                 = "tohu008"
+  metric_name               = "cart_count.value"
+  comparison_operator       = "GreaterThanThreshold"
+  threshold                 = "5"
+  evaluation_periods        = "5"
+  period                    = "300"
+  statistic                 = "Maximum"
+  alarm_description         = "This alarm goes off as soon as the total amount of carts exceeds 5 over 5 minutes"
+  insufficient_data_actions = []
+  alarm_actions       = [aws_sns_topic.alarms.arn]
+}
+
 resource "aws_sns_topic" "alarms" {
   name = "alarm-topic-${var.candidate_id}"
 }
