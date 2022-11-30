d8b829817e85edc2e49dc77ffc9200057fd46497
tonjehusvik <tonje.husvik@hotmail.com>
2022-11-30 22:01:36 +0100

    Workflow should work now and variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,9 +1,9 @@
 variable "candidate_id" {
   default = "1013"
-  type = string
+  type    = string
 }
 
-variable "candidate_email"  {
+variable "candidate_email" {
   default = "tohu008@student.kristiania.no"
-  type = string
+  type    = string
 }
